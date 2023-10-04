fecb2eb5bacc5240e9c319bc35b59a3befc8691f
codingrecipe1 <codingrecipe1@gmail.com>
2023-10-04 13:48:09 +0900

    02_글목록

--- src/main/java/com/icia/board/dto/BoardDTO.java
+++ src/main/java/com/icia/board/dto/BoardDTO.java
@@ -1,8 +1,11 @@
 package com.icia.board.dto;
 
+import com.icia.board.entity.BoardEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,6 +16,35 @@ public class BoardDTO {
     private String boardPass;
     private String boardTitle;
     private String boardContents;
-    private String createdAt;
+    private LocalDateTime createdAt;
     private int boardHits;
+
+    public static BoardDTO toDTO(BoardEntity boardEntity) {
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setCreatedAt(boardEntity.getCreatedAt());
+        return boardDTO;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/icia/board/service/BoardService.java
+++ src/main/java/com/icia/board/service/BoardService.java
@@ -6,6 +6,9 @@ import com.icia.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
@@ -14,6 +17,20 @@ public class BoardService {
     public Long save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         return boardRepository.save(boardEntity).getId();
+    }
+
+    public List<BoardDTO> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        boardEntityList.forEach(board -> {
+            boardDTOList.add(BoardDTO.toDTO(board));
+        });
+
+//        for (BoardEntity board: boardEntityList) {
+//            BoardDTO boardDTO = BoardDTO.toDTO(board);
+//            boardDTOList.add(boardDTO);
+//        }
+        return boardDTOList;
     }
 }
 
