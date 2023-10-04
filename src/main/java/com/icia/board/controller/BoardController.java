6e80c346bb7836731111a3c28abc901d0e3b92bc
codingrecipe1 <codingrecipe1@gmail.com>
2023-10-04 13:02:28 +0900

    Initial commit

---
+++ src/main/java/com/icia/board/controller/BoardController.java
@@ -0,0 +1,22 @@
+package com.icia.board.controller;
+
+import com.icia.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "boardPages/boardSave";
+    }
+
+
+}
---
+++ src/main/java/com/icia/board/service/BoardService.java
@@ -0,0 +1,11 @@
+package com.icia.board.service;
+
+import com.icia.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+}
