0e059e2da4d4cb0e6ba6211f11164f1b37d9320f
codingrecipe1 <codingrecipe1@gmail.com>
2023-10-04 15:55:03 +0900

    03_글조회

--- src/main/java/com/icia/board/controller/BoardController.java
+++ src/main/java/com/icia/board/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.icia.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,15 @@ public class BoardController {
         model.addAttribute("boardList", boardDTOList);
         return "boardPages/boardList";
     }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        boardService.increaseHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "boardPages/boardDetail";
+    }
+
 }
 
 
