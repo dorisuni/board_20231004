4f1f204afe56fa591840ea0e3950871d0c53c2a0
codingrecipe1 <codingrecipe1@gmail.com>
2023-10-05 15:37:44 +0900

    06_검색_메서드

--- src/test/java/com/icia/board/BoardTest.java
+++ src/test/java/com/icia/board/BoardTest.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -58,6 +59,45 @@ public class BoardTest {
         System.out.println("boardEntities.isLast() = " + boardEntities.isLast()); // 마지막페이지인지 여부
 
         // Page<BoardEntity> -> Page<BoardDTO>
+        Page<BoardDTO> boardList = boardEntities.map(boardEntity ->
+                BoardDTO.builder()
+                        .id(boardEntity.getId())
+                        .boardTitle(boardEntity.getBoardTitle())
+                        .boardWriter(boardEntity.getBoardWriter())
+                        .boardHits(boardEntity.getBoardHits())
+                        .createdAt(UtilClass.dateTimeFormat(boardEntity.getCreatedAt()))
+                        .build());
+        System.out.println("boardList.getContent() = " + boardList.getContent()); // 요청페이지에 들어있는 데이터
+        System.out.println("boardList.getTotalElements() = " + boardList.getTotalElements()); // 전체 글갯수
+        System.out.println("boardList.getNumber() = " + boardList.getNumber()); // 요청페이지(jpa 기준)
+        System.out.println("boardList.getTotalPages() = " + boardList.getTotalPages()); // 전체 페이지 갯수
+        System.out.println("boardList.getSize() = " + boardList.getSize()); // 한페이지에 보여지는 글갯수
+        System.out.println("boardList.hasPrevious() = " + boardList.hasPrevious()); // 이전페이지 존재 여부
+        System.out.println("boardList.isFirst() = " + boardList.isFirst()); // 첫페이지인지 여부
+        System.out.println("boardList.isLast() = " + boardList.isLast()); // 마지막페이지인지 여부
+    }
+
+    @Test
+    @DisplayName("검색 메서드 확인")
+    public void searchMethod() {
+        // 제목에 1이 포함된 결과 검색
+//        List<BoardEntity> boardEntityList = boardRepository.findByBoardTitleContainingOrderById("1");
+
+        // 제목에 1이 포함된 결과 페이징
+        int page = 0;
+        int pageLimit = 5;
+        Page<BoardEntity> boardEntities =
+                boardRepository.findByBoardTitleContaining("1", PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+        // 제목 또는 작성자에 1이 포함된 결과 페이징
+        String q = "1";
+        boardEntities = boardRepository.findByBoardTitleContainingOrBoardWriterContaining(q, q, PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+
+
+        // BoardEntity를 출력하지 않고 각각을 DTO로 변환해서 출력
+//        boardEntityList.forEach(boardEntity -> {
+//            System.out.println(BoardDTO.toDTO(boardEntity));
+//        });
+
         Page<BoardDTO> boardList = boardEntities.map(boardEntity ->
                 BoardDTO.builder()
                         .id(boardEntity.getId())
